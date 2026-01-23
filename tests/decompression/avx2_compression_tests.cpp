0e42db6174e2145c7b02489978d6e13f79c1c0d6
Felix Sternberg <felix@sternberg.cx>
2026-01-23 19:51:48 +0100

    Rewrite AVX2 and AVX512 VBMI compression and decompression tests

---
+++ tests/decompression/avx2_compression_tests.cpp
@@ -0,0 +1,25 @@
+#include <pernix/compression.h>
+#include <testset.h>
+
+#ifdef PERNIX_AVX2_ENABLED
+
+TYPED_TEST(DecompressionTest, AVX2DecompressBlock) {
+    std::vector<std::vector<float_t>> decompressedData(this->testSet.numberOfBlocks);
+
+    for (uint32_t block = 0; block < this->testSet.numberOfBlocks; block++) {
+        decompressedData[block].resize(this->testSet.elementsPerBlock);
+
+        pernix::mm256_decompress_block_avx2<this->BitWidth>(this->testSet.getCompressedData()[block].data(),
+                                                            this->testSet.getScales()[block], decompressedData[block].data());
+    }
+
+    for (uint32_t block = 0; block < this->testSet.numberOfBlocks; block++) {
+        std::vector<float_t>& data = decompressedData[block];
+
+        for (uint32_t i = 0; i < data.size(); i++) {
+            ASSERT_NEAR(decompressedData[block][i], decompressedData[block][i], this->testSet.getScales()[block] / 2);
+        }
+    }
+}
+
+#endif  // PERNIX_AVX2_ENABLED
---
+++ tests/decompression/fallback_compression_tests.cpp
@@ -0,0 +1,21 @@
+#include <pernix/compression.h>
+#include <testset.h>
+
+TYPED_TEST(DecompressionTest, FallbackDecompressBlock) {
+    std::vector<std::vector<float_t>> decompressedData(this->testSet.numberOfBlocks);
+
+    for (uint32_t block = 0; block < this->testSet.numberOfBlocks; block++) {
+        decompressedData[block].resize(this->testSet.elementsPerBlock);
+
+        pernix::decompress_block_fallback<this->BitWidth>(this->testSet.getCompressedData()[block].data(), this->testSet.getScales()[block],
+                                                          decompressedData[block].data());
+    }
+
+    for (uint32_t block = 0; block < this->testSet.numberOfBlocks; block++) {
+        std::vector<float_t>& data = decompressedData[block];
+
+        for (uint32_t i = 0; i < data.size(); i++) {
+            ASSERT_NEAR(decompressedData[block][i], decompressedData[block][i], this->testSet.getScales()[block] / 2);
+        }
+    }
+}

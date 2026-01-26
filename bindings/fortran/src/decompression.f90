ceed3f1d90c2e23efd41f086dc08aae2219ffe37
Felix Sternberg <felix@sternberg.cx>
2026-01-26 20:48:04 +0100

    Refactor compression and decompression subroutines to include block handling for AVX2 and AVX512

--- bindings/fortran/src/decompression.f90
+++ bindings/fortran/src/decompression.f90
@@ -3,6 +3,10 @@ module pernix_decompression
     implicit none
 
     interface
+
+#if (__SSE3__ && __SSE4_1__ && __SSE4_2__)
+#if (__AVX__ && __AVX2__ && __FMA__ && __BMI__ && __BMI2__)
+
         subroutine mm256_decompress_block_bmi2(bit_width, input_ptr, scale, output_ptr) bind(C, name = "mm256_decompress_block_bmi2")
             import :: c_int8_t, c_float, c_ptr
             integer(c_int8_t), value :: bit_width
@@ -12,22 +16,6 @@ module pernix_decompression
         end subroutine
 
         subroutine mm256_decompress_block_avx2(bit_width, input_ptr, scale, output_ptr) bind(C, name = "mm256_decompress_block_avx2")
-            import :: c_int8_t, c_float, c_ptr
-            integer(c_int8_t), value :: bit_width
-            type(c_ptr), value :: input_ptr
-            real(c_float), value :: scale
-            type(c_ptr), value :: output_ptr
-        end subroutine
-
-        subroutine mm512_decompress_block_avx512vbmi(bit_width, input_ptr, scale, output_ptr) bind(C, name = "mm512_decompress_block_avx512vbmi")
-            import :: c_int8_t, c_float, c_ptr
-            integer(c_int8_t), value :: bit_width
-            type(c_ptr), value :: input_ptr
-            real(c_float), value :: scale
-            type(c_ptr), value :: output_ptr
-        end subroutine
-
-        subroutine decompress_block_fallback(bit_width, input_ptr, scale, output_ptr) bind(C, name = "decompress_block_fallback")
             import :: c_int8_t, c_float, c_ptr
             integer(c_int8_t), value :: bit_width
             type(c_ptr), value :: input_ptr
@@ -54,6 +42,16 @@ module pernix_decompression
             integer(c_int32_t), value :: blocks
         end subroutine
 
+#if (__AVX512BW__ && __AVX512CD__ && __AVX512DQ__ && __AVX512F__ && __AVX512VL__ && __AVX512VBMI__)
+
+        subroutine mm512_decompress_block_avx512vbmi(bit_width, input_ptr, scale, output_ptr) bind(C, name = "mm512_decompress_block_avx512vbmi")
+            import :: c_int8_t, c_float, c_ptr
+            integer(c_int8_t), value :: bit_width
+            type(c_ptr), value :: input_ptr
+            real(c_float), value :: scale
+            type(c_ptr), value :: output_ptr
+        end subroutine
+
         subroutine mm512_decompress_blocks_avx512vbmi(bit_width, input_ptr, scale, output_ptr, blocks) bind(C, name = "mm512_decompress_blocks_avx512vbmi")
             import :: c_int8_t, c_int32_t, c_float, c_ptr
             integer(c_int8_t), value :: bit_width
@@ -61,6 +59,18 @@ module pernix_decompression
             real(c_float), value :: scale
             type(c_ptr), value :: output_ptr
             integer(c_int32_t), value :: blocks
+        end subroutine
+
+#endif
+#endif
+#endif
+
+        subroutine decompress_block_fallback(bit_width, input_ptr, scale, output_ptr) bind(C, name = "decompress_block_fallback")
+            import :: c_int8_t, c_float, c_ptr
+            integer(c_int8_t), value :: bit_width
+            type(c_ptr), value :: input_ptr
+            real(c_float), value :: scale
+            type(c_ptr), value :: output_ptr
         end subroutine
 
         subroutine decompress_blocks_fallback(bit_width, input_ptr, scale, output_ptr, blocks) bind(C, name = "decompress_blocks_fallback")
@@ -71,7 +81,6 @@ module pernix_decompression
             type(c_ptr), value :: output_ptr
             integer(c_int32_t), value :: blocks
         end subroutine
-
 
     end interface
 

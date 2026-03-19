435cf7039ba3e704f72fa60a1ccb0bb005f19d04
Felix Sternberg <felix@sternberg.cx>
2026-03-19 14:54:48 +0100

    standardize avx2 shuffle tables and permute tables; add a computing function for shift table

--- include/pernix/avx2/tables.h
+++ include/pernix/avx2/tables.h
@@ -712,27 +712,14 @@ template <uint8_t BIT_WIDTH, typename T>
 struct unpack_tables_avx2 {
     alignas(32) inline static constexpr std::array<int32_t, 8> permute = [] {
         // clang-format off
-        if constexpr (BIT_WIDTH == 1 || BIT_WIDTH == 2 || BIT_WIDTH == 3 || BIT_WIDTH == 4) {
-            return std::array{0, -1, -1, -1, 0, -1, -1, -1};
-        } else if constexpr (BIT_WIDTH == 5 || BIT_WIDTH == 6 || BIT_WIDTH == 7) {
+        if constexpr (BIT_WIDTH >= 1 && BIT_WIDTH <= 8) {
             return std::array{0, -1, -1, -1, 0, 1, -1, -1};
-        } else if constexpr (BIT_WIDTH == 8) {
-            return std::array{0, -1, -1, -1, 1, 0, -1, -1};
-        } else if constexpr (BIT_WIDTH == 9 || BIT_WIDTH == 10 || BIT_WIDTH == 11 || BIT_WIDTH == 12) {
-            return std::array{0, 1, -1, -1, 1, 2, -1, -1};
-        } else if constexpr (BIT_WIDTH == 13 || BIT_WIDTH == 14 || BIT_WIDTH == 15) {
+        } else if constexpr (BIT_WIDTH >= 9 && BIT_WIDTH <= 16) {
             return std::array{0, 1, -1, -1, 1, 2, 3, -1};
-        } else if constexpr (BIT_WIDTH == 16) {
-            return std::array{0, 1, -1, -1, 2, 3, -1, -1};
-        } else if constexpr (BIT_WIDTH == 17 || BIT_WIDTH == 18 || BIT_WIDTH == 19 || BIT_WIDTH == 20) {
-            return std::array{0, 1, 2, -1, 2, 3, 4, -1};
-        } else if constexpr (BIT_WIDTH == 21 || BIT_WIDTH == 22 || BIT_WIDTH == 23) {
+        } else if constexpr (BIT_WIDTH >= 17 && BIT_WIDTH <= 24) {
             return std::array{0, 1, 2, -1, 2, 3, 4, 5};
-        } else if constexpr (BIT_WIDTH == 24) {
-            return std::array{0, 1, 2, -1, 3, 4, 5, -1};
         }
         // clang-format on
-        return std::array<int32_t, 8>{};
     }();
 
     alignas(32) inline static constexpr std::array<int8_t, 32> shuffle = [] {
@@ -828,10 +815,10 @@ struct unpack_tables_avx2 {
                 2, -1, -1, -1,
                 3, -1, -1, -1,
 
-                0, -1, -1, -1,
-                1, -1, -1, -1,
-                2, -1, -1, -1,
-                3, -1, -1, -1
+                4, -1, -1, -1,
+                5, -1, -1, -1,
+                6, -1, -1, -1,
+                7, -1, -1, -1
             };
         } else if constexpr (BIT_WIDTH == 9) {
             return std::array<int8_t, 32>{
@@ -924,10 +911,10 @@ struct unpack_tables_avx2 {
                 4, 5, -1, -1,
                 6, 7, -1, -1,
 
-                0, 1, -1, -1,
-                2, 3, -1, -1,
                 4, 5, -1, -1,
                 6, 7, -1, -1,
+                8, 9, -1, -1,
+                10, 11, -1, -1,
             };
         } else if constexpr (BIT_WIDTH == 17) {
             return std::array<int8_t, 32>{
@@ -1020,117 +1007,26 @@ struct unpack_tables_avx2 {
                 6,  7,  8, -1,
                 9, 10, 11, -1,
 
-                0,  1,  2, -1,
-                3,  4,  5, -1,
-                6,  7,  8, -1,
-                9, 10, 11, -1,
+                4,  5,  6, -1,
+                7,  8,  9, -1,
+                10, 11, 12, -1,
+                13, 14, 15, -1,
             };
         }
         // clang-format on
-        return std::array<int8_t, 32>{};
     }();
 
     alignas(64) inline static constexpr std::array<int32_t, 8> shift = [] {
-        // clang-format off
-        if constexpr (BIT_WIDTH == 1) {
-            return std::array{
-                31, 30, 29, 28, 27, 26, 25, 24,
-            };
-        } else if constexpr (BIT_WIDTH == 2) {
-            return std::array{
-                30, 28, 26, 24, 30, 28, 26, 24,
-            };
-        } else if constexpr (BIT_WIDTH == 3) {
-            return std::array{
-                29, 26, 23, 28, 25, 22, 27, 24,
-            };
-        } else if constexpr (BIT_WIDTH == 4) {
-            return std::array{
-                28, 24, 28, 24, 28, 24, 28, 24,
-            };
-        } else if constexpr (BIT_WIDTH == 5) {
-            return std::array{
-                27, 22, 25, 20, 23, 26, 21, 24,
-            };
-        } else if constexpr (BIT_WIDTH == 6) {
-            return std::array{
-                26, 20, 22, 24, 26, 20, 22, 24,
-            };
-        } else if constexpr (BIT_WIDTH == 7) {
-            return std::array{
-                25, 18, 19, 20, 21, 22, 23, 24,
-            };
-        } else if constexpr (BIT_WIDTH == 8) {
-            return std::array{
-                24, 24, 24, 24, 24, 24, 24, 24,
-            };
-        } else if constexpr (BIT_WIDTH == 9) {
-            return std::array{
-                23, 22, 21, 20, 19, 18, 17, 16,
-            };
-        } else if constexpr (BIT_WIDTH == 10) {
-            return std::array{
-                22, 20, 18, 16, 22, 20, 18, 16,
-            };
-        } else if constexpr (BIT_WIDTH == 11) {
-            return std::array{
-                21, 18, 15, 20, 17, 14, 19, 16,
-            };
-        } else if constexpr (BIT_WIDTH == 12) {
-            return std::array{
-                20, 16, 20, 16, 20, 16, 20, 16,
-            };
-        } else if constexpr (BIT_WIDTH == 13) {
-            return std::array{
-                19, 14, 17, 12, 15, 18, 13, 16,
-            };
-        } else if constexpr (BIT_WIDTH == 14) {
-            return std::array{
-                18, 12, 14, 16, 18, 12, 14, 16,
-            };
-        } else if constexpr (BIT_WIDTH == 15) {
-            return std::array{
-                17, 10, 11, 12, 13, 14, 15, 16,
-            };
-        } else if constexpr (BIT_WIDTH == 16) {
-            return std::array{
-                16, 16, 16, 16, 16, 16, 16, 16,
-            };
-        } else if constexpr (BIT_WIDTH == 17) {
-            return std::array{
-                15, 14, 13, 12, 11, 10, 9, 8,
-            };
-        } else if constexpr (BIT_WIDTH == 18) {
-            return std::array{
-                14, 12, 10, 8, 14, 12, 10, 8,
-            };
-        } else if constexpr (BIT_WIDTH == 19) {
-            return std::array{
-                13, 10, 7, 12, 9, 6, 11, 8,
-            };
-        } else if constexpr (BIT_WIDTH == 20) {
-            return std::array{
-                12, 8, 12, 8, 12, 8, 12, 8,
-            };
-        } else if constexpr (BIT_WIDTH == 21) {
-            return std::array{
-                11, 6, 9, 4, 7, 10, 5, 8,
-            };
-        } else if constexpr (BIT_WIDTH == 22) {
-            return std::array{
-                10, 4, 6, 8, 10, 4, 6, 8,
-            };
-        } else if constexpr (BIT_WIDTH == 23) {
-            return std::array{
-                9, 2, 3, 4, 5, 6, 7, 8,
-            };
-        } else if constexpr (BIT_WIDTH == 24) {
-            return std::array{
-                8, 8, 8, 8, 8, 8, 8, 8,
-            };
+        std::array<int32_t, 8> shifts{};
+
+        for (std::size_t lane = 0; lane < 8; ++lane) {
+            const int bit_offset  = lane * BIT_WIDTH;
+            const int bit_in_byte = bit_offset % 8;
+            const int left_shift  = 32 - BIT_WIDTH - bit_in_byte;
+            shifts[lane]          = left_shift;
         }
-        // clang-format on
-        return std::array<int32_t, 8>{};
+
+        return shifts;
     }();
 
 #pragma GCC diagnostic push
